2e4a7e8c5cb2588fb3e27f1f8d65875a13f871fb
Kavin Prakash Krishnan <kavinprakashkrishnan@Kavins-MacBook-Air.local>
2026-03-31 10:33:37 +0530

    implemented bcrypt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const bcrypt = require("bcrypt")
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -25,7 +26,6 @@ const registerUser = async (req, res) => {
         if (userExists) {
             return res.status(400).json({ message: 'User already exists' });
         }
-
         const user = await User.create({
             name,
             email,
@@ -54,16 +54,23 @@ const authUser = async (req, res) => {
     try {
         const user = await User.findOne({ email });
 
-        if (user && (await user.matchPassword(password))) {
-            res.json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                token: generateToken(user._id),
-            });
-        } else {
-            res.status(401).json({ message: 'Invalid email or password' });
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
         }
+
+        const isMatch = await bcrypt.compare(password.trim(), user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+
+        return res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id), // 🔥 important
+        });
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
